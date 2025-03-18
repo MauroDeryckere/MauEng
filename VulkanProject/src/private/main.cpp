4cb230bd70fd49457d10e664f7189e7f20c9f393
Mauro Deryckere <mauro.deryckere@gmail.com>
2025-03-18 04:46:01 +0100

    Image views

--- VulkanProject/src/private/main.cpp
+++ VulkanProject/src/private/main.cpp
@@ -42,9 +42,10 @@ VkSurfaceKHR g_WindowSurface;
 VkSwapchainKHR g_SwapChain;
 
 std::vector<VkImage> g_SwapChainImages;
-
 VkFormat g_SwapChainImageFormat;
 VkExtent2D g_SwapChainExtent;
+
+std::vector<VkImageView> g_SwapChainImageViews;
 
 #ifdef NDEBUG
 	bool constexpr ENABLE_VULKAN_VALIDATION_LAYERS{ false };
@@ -744,6 +745,32 @@ void CreateWindowSurface()
 	}
 }
 
+void CreateImageViews()
+{
+	g_SwapChainImageViews.resize(g_SwapChainImages.size());
+	for (size_t i = 0; i < g_SwapChainImages.size(); i++) 
+	{
+		VkImageViewCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+		createInfo.image = g_SwapChainImages[i];
+		createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+		createInfo.format = g_SwapChainImageFormat;
+		createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+		createInfo.subresourceRange.baseMipLevel = 0;
+		createInfo.subresourceRange.levelCount = 1;
+		createInfo.subresourceRange.baseArrayLayer = 0;
+		createInfo.subresourceRange.layerCount = 1;
+		if (vkCreateImageView(g_LogicalDevice, &createInfo, nullptr, &g_SwapChainImageViews[i]) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create image views!");
+		}
+	}
+}
+
 void InitVulkan()
 {
 	CreateVulkanInstance();
@@ -752,7 +779,7 @@ void InitVulkan()
 	SelectPhysicalDevice();
 	CreateLogicalDevice();
 	CreateSwapChain();
-
+	CreateImageViews();
 }
 
 void MainLoop()
@@ -766,6 +793,11 @@ void MainLoop()
 
 void Cleanup()
 {
+	for (auto const& imageView : g_SwapChainImageViews) 
+	{
+		vkDestroyImageView(g_LogicalDevice, imageView, nullptr);
+	}
+
 	vkDestroySwapchainKHR(g_LogicalDevice, g_SwapChain, nullptr);
 
 	vkDestroyDevice(g_LogicalDevice, nullptr);
