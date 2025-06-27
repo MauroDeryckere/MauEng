fd79b78b6913ec95470617a8d3cc2611d51c3b5f
Mauro Deryckere <mauro.deryckere@gmail.com>
2025-06-27 06:03:06 +0200

    [Refactor] handler specialization (void) for no params

--- Engine/Core/Public/Events/ListenerHandlers.h
+++ Engine/Core/Public/Events/ListenerHandlers.h
@@ -8,7 +8,7 @@ namespace MauCor
 	template<typename ParamType, typename Callable>
 	concept CallableWithParam = std::invocable<Callable, ParamType const&>;
 
-	template<typename ParamType>
+	template<typename ParamType = void>
 	class IListenerHandler
 	{
 	public:
@@ -27,8 +27,27 @@ namespace MauCor
 	protected:
 		ListenerHandle m_ListenerHandle;
 	};
+	template<>
+	class IListenerHandler<void>
+	{
+	public:
+		explicit IListenerHandler(ListenerHandle const& handle) : m_ListenerHandle{ handle } {}
+		virtual ~IListenerHandler() = default;
 
-	template<typename ParamType>
+		virtual void Invoke() const noexcept = 0;
+		[[nodiscard]] virtual bool IsValid() const noexcept = 0;
+
+		[[nodiscard]] ListenerHandle const& GetHandle() const noexcept { return m_ListenerHandle; }
+
+		IListenerHandler(IListenerHandler const&) = delete;
+		IListenerHandler(IListenerHandler&&) = delete;
+		IListenerHandler& operator=(IListenerHandler const&) = delete;
+		IListenerHandler& operator=(IListenerHandler&&) = delete;
+	protected:
+		ListenerHandle m_ListenerHandle;
+	};
+
+	template<typename ParamType = void>
 	class CallableHandler final : public IListenerHandler<ParamType>
 	{
 	public:
@@ -52,8 +71,31 @@ namespace MauCor
 	private:
 		std::function<void(ParamType const&)> m_Callback;
 	};
+	template<>
+	class CallableHandler<void> final : public IListenerHandler<void>
+	{
+	public:
+		template<typename Callable>
+		explicit CallableHandler(ListenerHandle const& handle, Callable&& cb) :
+			IListenerHandler{ handle },
+			m_Callback{ std::forward<Callable>(cb) } {
+		}
 
-	template<typename T, typename ParamType>
+		~CallableHandler() override = default;
+
+		virtual void Invoke() const noexcept override { m_Callback(); }
+		[[nodiscard]] virtual bool IsValid() const noexcept override { return true; }
+
+		CallableHandler(CallableHandler const&) = delete;
+		CallableHandler(CallableHandler&&) = delete;
+		CallableHandler& operator=(CallableHandler const&) = delete;
+		CallableHandler& operator=(CallableHandler&&) = delete;
+
+	private:
+		std::function<void()> m_Callback;
+	};
+
+	template<typename T, typename ParamType = void>
 	class MemberFunHandler final : public IListenerHandler<ParamType>
 	{
 	public:
@@ -79,8 +121,34 @@ namespace MauCor
 		T* m_pObject;
 		MemFnType m_MemFn;
 	};
+	template<typename T>
+	class MemberFunHandler<T, void> final : public IListenerHandler<void>
+	{
+	public:
+		using MemFnType = void (T::*)();
 
-	template<typename T, typename ParamType>
+		explicit MemberFunHandler(ListenerHandle const& handle, T* obj, MemFnType fn) :
+			IListenerHandler{ handle },
+			m_pObject{ obj },
+			m_MemFn{ fn } {
+		}
+
+		~MemberFunHandler() override = default;
+
+		virtual void Invoke() const noexcept override { (m_pObject->*m_MemFn)(); }
+		[[nodiscard]] virtual bool IsValid() const noexcept override { return m_pObject != nullptr; }
+
+		MemberFunHandler(MemberFunHandler const&) = delete;
+		MemberFunHandler(MemberFunHandler&&) = delete;
+		MemberFunHandler& operator=(MemberFunHandler const&) = delete;
+		MemberFunHandler& operator=(MemberFunHandler&&) = delete;
+
+	private:
+		T* m_pObject;
+		MemFnType m_MemFn;
+	};
+
+	template<typename T, typename ParamType = void>
 	class MemberFunHandlerConst final : public IListenerHandler<ParamType>
 	{
 	public:
@@ -95,6 +163,34 @@ namespace MauCor
 		void Invoke(ParamType const& e) const noexcept override
 		{
 			(m_Object->*m_MemFn)(e);
+		}
+
+		bool IsValid() const noexcept override { return m_Object != nullptr; }
+
+		MemberFunHandlerConst(MemberFunHandlerConst const&) = delete;
+		MemberFunHandlerConst(MemberFunHandlerConst&&) = delete;
+		MemberFunHandlerConst& operator=(MemberFunHandlerConst const&) = delete;
+		MemberFunHandlerConst& operator=(MemberFunHandlerConst&&) = delete;
+
+	private:
+		T const* m_Object;
+		MemFnType m_MemFn;
+	};
+	template<typename T>
+	class MemberFunHandlerConst<T, void> final : public IListenerHandler<void>
+	{
+	public:
+		using MemFnType = void (T::*)() const;
+
+		MemberFunHandlerConst(ListenerHandle const& handle, T const* obj, MemFnType fn) :
+			IListenerHandler{ handle },
+			m_Object{ obj },
+			m_MemFn{ fn } {
+		}
+
+		void Invoke() const noexcept override
+		{
+			(m_Object->*m_MemFn)();
 		}
 
 		bool IsValid() const noexcept override { return m_Object != nullptr; }
